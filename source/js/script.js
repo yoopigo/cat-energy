534127fb85960d4a63ff31bae4df60c201028ce0
yoopigo <collerggggggg@gmail.com>
2022-08-11 19:44:32 +0500

    JS

--- source/js/script.js
+++ source/js/script.js
@@ -18,6 +18,47 @@ navToggle.addEventListener("click", function (evt) {
 
 //InputMask
 
-let inputPhone = document.querySelectorAll('input[type="tel"]');
-let im = new Inputmask("8 (999) 999-99-99");
-im.mask(inputPhone);
+var maskedInputs = document.querySelectorAll("[data-mask]");
+
+for (var index = 0; index < maskedInputs.length; index++) {
+  maskedInputs[index].addEventListener("input", maskInput);
+}
+
+function maskInput() {
+  var input = this;
+  var mask = input.dataset.mask;
+  console.log(mask);
+  var value = input.value;
+  var literalPattern = /[0\*]/;
+  var numberPattern = /[0-9]/;
+  var newValue = "";
+  try {
+    var maskLength = mask.length;
+    var valueIndex = 0;
+    var maskIndex = 0;
+
+    for (; maskIndex < maskLength; ) {
+      if (maskIndex >= value.length) break;
+
+      if (
+        mask[maskIndex] === "0" &&
+        value[valueIndex].match(numberPattern) === null
+      )
+        break;
+
+      // Found a literal
+      while (mask[maskIndex].match(literalPattern) === null) {
+        if (value[valueIndex] === mask[maskIndex]) break;
+        newValue += mask[maskIndex++];
+      }
+      newValue += value[valueIndex++];
+      maskIndex++;
+    }
+
+    input.value = newValue;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+///
